5feb8d4e992ab724bc2e1c5a689c786839cd3884
marianntapfer <tapferm@gmail.com>
2021-01-26 11:53:25 +0200

    StrapiHost on StrapiHostPoff2021

--- ssg/helpers/strapiAuth.js
+++ ssg/helpers/strapiAuth.js
@@ -4,14 +4,14 @@ const http = require('http')
 async function strapiAuth() {
 
     return new Promise((resolve, reject) => {
-        console.log("muutujad",process.env['StrapiUserName'],process.env['StrapiPassword'],process.env['StrapiHost'])
+        console.log("muutujad",process.env['StrapiUserName'],process.env['StrapiPassword'],process.env['StrapiHostPoff2021'])
         const postData = {
             identifier: process.env['StrapiUserName'],
             password: process.env['StrapiPassword']
         }
 
         const options = {
-            hostname: process.env['StrapiHost'],
+            hostname: process.env['StrapiHostPoff2021'],
             path: '/auth/local',
             method: 'POST',
             headers: {
