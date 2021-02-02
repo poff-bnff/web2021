dddc81890769d68f0b7fe3e88ad3aa76096f1688
SiimS1 <siimsutt@hotmail.com>
2021-02-02 14:03:20 +0000

    copy live

--- strapi/strapi-development/api/hof-fi-article/models/hof-fi-article.js
+++ strapi/strapi-development/api/hof-fi-article/models/hof-fi-article.js
@@ -1,8 +1,35 @@
+
 'use strict';
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const { execFile } = require('child_process');
+const fs = require('fs');
+const yaml = require('yaml');
+const path = require('path');
 
-module.exports = {};
+
+
+module.exports = {
+	lifecycles: {
+		beforeUpdate(params, data) {
+			console.log(data.published_at)
+		},
+		afterUpdate(result, params, data) {
+			if (result.published_at) {
+				if (fs.existsSync(`/srv/ssg/build_hoff.sh`)) {
+					console.log('START FULLBUILD')
+					const child = execFile('bash', [`/srv/ssg/build_hoff.sh`, result.id], (error, stdout, stderr) => {
+						if (error) {
+							throw error;
+						}
+						console.log(stdout);
+					})
+				}
+			}
+		}
+	}
+};
