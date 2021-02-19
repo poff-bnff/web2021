d1dd9d2a1a3efdc67c98b89d1d278b434cb570ab
SiimS1 <siimsutt@hotmail.com>
2021-02-19 15:50:17 +0000

    Katkised mudelid jmt

--- strapi/strapi-development/api/trio-hoff/models/trio-hoff.js
+++ strapi/strapi-development/api/trio-hoff/models/trio-hoff.js
@@ -22,26 +22,24 @@ const path = require('path');
         beforeUpdate(params, data) {
         },
         afterUpdate(result, params, data) {
-            if (result.published_at) {
-                if (fs.existsSync('/srv/ssg/build_hoff.sh')) {
-                    const args = []
+            if (fs.existsSync('/srv/ssg/build_hoff.sh')) {
+                const args = []
 
-                    const child = spawn('/srv/ssg/build_hoff.sh', args)
+                const child = spawn('/srv/ssg/build_hoff.sh', args)
 
-                    child.stdout.on('data', (chunk) => {
-                        console.log(decoder.write(chunk))
-                      // data from the standard output is here as buffers
-                    });
-                    // since these are streams, you can pipe them elsewhere
-                    child.stderr.on('data', (chunk) => {
-                        console.log('err:', decoder.write(chunk))
-                      // data from the standard error is here as buffers
-                    });
-                    // child.stderr.pipe(child.stdout);
-                    child.on('close', (code) => {
-                        console.log(`child process exited with code ${code}`);
-                    });
-                }
+                child.stdout.on('data', (chunk) => {
+                    console.log(decoder.write(chunk))
+                  // data from the standard output is here as buffers
+                });
+                // since these are streams, you can pipe them elsewhere
+                child.stderr.on('data', (chunk) => {
+                    console.log('err:', decoder.write(chunk))
+                  // data from the standard error is here as buffers
+                });
+                // child.stderr.pipe(child.stdout);
+                child.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                });
             }
         }
     }
