181dd313a96e30c612497f7f91aae45635f1f3dd
marianntapfer <tapferm@gmail.com>
2021-03-08 12:36:40 +0200

    full build button started

--- strapi/strapi-development/plugins/publisher/admin/src/containers/HomePage/index.js
+++ strapi/strapi-development/plugins/publisher/admin/src/containers/HomePage/index.js
@@ -4,6 +4,19 @@ import ButtonAndLog from "./HomePageContent/ButtonAndLog";
 import { Header } from '@buffetjs/custom';
 import '../../assets/style.css';
 import Container from '../../components/Container';
+import Button from '../../../components/Button';
+
+const = DoBuildFull() => {
+	console.log("full build")
+	//post päring plugina back-i 
+	   //  const response = await fetch(`${strapi.backendURL}/publisher/publish`, {
+    //   method: 'POST',
+    //   headers: {
+    //   	'Content-Type': 'application/json'
+    //    },
+    //   body: JSON.stringify({site: site, userInfo: userInfo})
+    // })
+}
 
 
 const HomePage = () => {
@@ -14,6 +27,7 @@ const HomePage = () => {
      	 	<ButtonAndLog site="hoff.ee" buttonText="HÕFF LIVE"/>
      	 	<ButtonAndLog site="kumu.poff.ee" buttonText="KUMU LIVE"/>
      	 	<ButtonAndLog site="filmikool.poff.ee" buttonText="FILMIKOOL LIVE"/>
+     	 	<Button color="primary" onClick={() => DoBuildFull(site, userInfo)}>Full build</Button>
     </Container>
   );
 };
