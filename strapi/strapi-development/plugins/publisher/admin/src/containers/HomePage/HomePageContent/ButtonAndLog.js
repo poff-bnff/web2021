4211d9fdfa0fc338c725f0dc91a8f8acc924e2a7
Liis <liis.kasper@gmail.com>
2021-03-10 21:17:42 +0200

    missing file

---
+++ strapi/strapi-development/plugins/publisher/admin/src/containers/HomePage/HomePageContent/ButtonAndLog.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import ListOfLogs from "./ListOfLogs";
+import Button from '../../../components/Button';
+import LogsAccordion from "./LogsAccordion";
+import { Flex, Text } from '@buffetjs/core';
+
+
+
+const DoPublish = async (site, userInfo) => {
+
+    const response = await fetch(`${strapi.backendURL}/publisher/publish`, {
+      method: 'POST',
+      headers: {
+      	'Content-Type': 'application/json'
+       },
+      body: JSON.stringify({site: site, userInfo: userInfo})
+    })
+
+    //link:{url:"/logs", label: "vaata logi"},
+
+    let myResult = await response.json();
+    // console.log(myResult)
+    if(response.status === 200){
+		strapi.notification.toggle({type: "success", message: `${myResult.message}`, title: "HÕFF", timeout: 5000, blockTransition: false})
+    }else if( response.status === 429){
+    	// setErrorStatus(true)
+	    if(Array.isArray(myResult.message)){
+	    	myResult=myResult.message[0].messages[0]
+	    	strapi.notification.toggle({type: "warning", message: `${myResult.message}`, title: "HÕFF", timeout: 5000, blockTransition: false})
+	    	// setPublishResult(myResult.message)
+	    }
+    }else{
+	    if(response.statusText && response.status){
+	    	// setPublishResult(`${response.status}: ${response.statusText}`)
+	    	strapi.notification.toggle({type: "warning", message: `${myResult.message}`, title: "HÕFF", timeout: 5000, blockTransition: false})
+	    }
+    }
+}
+
+const ButtonAndLog = ({site, buttonText}) => {
+	let userInfo
+	if(sessionStorage.getItem("userInfo")){
+		userInfo = sessionStorage.getItem("userInfo")
+	}else if(localStorage.getItem("userInfo")){
+		userInfo = localStorage.getItem("userInfo")
+	}else{
+		console.log("pole userinfot")
+	}
+
+	//title kohale viimane logi ja content kohale tabel logidest
+
+	return (
+		<div className="btn-and-log">
+		    <Button color="primary" onClick={() => DoPublish(site, userInfo)}>{buttonText}</Button>
+		    <LogsAccordion site={site}/>
+          	{/**<ListOfLogs site={site}/>**/}
+    	</div>
+	);
+};
+
+export default ButtonAndLog;
