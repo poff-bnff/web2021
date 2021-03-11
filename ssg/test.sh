b23c0daa428df6a2735a5d4927a6b0b47be71d62
SiimS1 <siimsutt@hotmail.com>
2021-03-11 11:41:40 +0000

    Commit

---
+++ ssg/test.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+/bin/echo "Welcome!
+Your choices are:
+1    Git pull
+----------------------------
+2    Build hoff.ee
+3    Build filmikool.poff.ee
+----------------------------
+4    LIVE Hoff.ee
+5    LIVE filmikool.poff.ee
+----------------------------
+q    Quit"
+
+/bin/echo
+/bin/echo "Your choice:"
+
+read ans
+
+while [ "$ans" != "q" ]
+do
+   case "$ans" in
+      1)
+         git pull
+         ;;
+      2)
+         bash /srv/ssg/build_hoff.sh
+         ;;
+      3)
+         /bin/echo "Not yet available" 
+         ;;
+      4)
+         bash /srv/ssg/deploy.sh hoff.ee
+         ;;
+      5) 
+         /bin/echo "Not yet available"
+         ;;       
+      q)
+         /bin/echo "Goodbye"
+         exit 0
+         ;;
+      *)
+         /bin/echo "Invalid choice '$ans': please try again"
+         ;;
+   esac
+/bin/echo
+/bin/echo "Your choices are:
+1    Git pull
+-------------------------------
+2    Build hoff.ee
+3    Build filmikool.poff.ee
+-------------------------------
+4    LIVE hoff.ee
+5    LIVE filmikool.poff.ee
+-------------------------------
+q    Quit"
+
+/bin/echo
+/bin/echo "Your choice:"
+
+   read ans
+done
+exit 0
