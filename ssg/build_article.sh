d1d6c58d9a483ccccbaf566308a2a2c2f2b2d9c0
SiimS1 <siimsutt@hotmail.com>
2021-03-12 13:24:21 +0200

    build article everydir

--- ssg/build_article.sh
+++ ssg/build_article.sh
@@ -2,39 +2,35 @@
 
 SECONDS=0
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
-echo "$PWD"
-
 THISDIR=${PWD##*/}
 
 # if [ "$THISDIR" != "ssg" ]; then
 #     cd "/srv/ssg"
 # fi
 
-echo $PWD
-
 export DOMAIN="$1"
 
 TARGET=$2
-echo TARGET
-echo $TARGET
+# echo TARGET
+# echo $TARGET
 id=$3
-echo id
-echo $id
+# echo id
+# echo $id
+echo `node -v` node
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+FETCH_PATH=`pwd`/helpers
+BUILD_PATH=`pwd`
+echo "$FETCH_PATH" FETCH_PATH
 
-
-# LOCAL=/home/liis/Documents/web2021/ssg
-
-node ./helpers/fetch_articles_from_yaml.js $TARGET $id
-node ./helpers/fetch_article_type_from_yaml.js  $TARGET $id
-node ./helpers/fetch_heroarticle_from_yaml.js $TARGET $id
-node ./helpers/fetch_menu_from_yaml.js $TARGET $id
-node ./helpers/fetch_trioblock_from_yaml.js $TARGET $id
-
-node ./node_modules/entu-ssg/src/build.js ./entu-ssg.yaml full
+node $FETCH_PATH/fetch_articles_from_yaml.js $TARGET $id 
+node "$FETCH_PATH"/fetch_article_type_from_yaml.js  $TARGET $id
+node "$FETCH_PATH"/fetch_heroarticle_from_yaml.js $TARGET $id
+node "$FETCH_PATH"/fetch_menu_from_yaml.js $TARGET $id
+# node "$SCRIPT_PATH"/fetch_trioblock_from_yaml.js $TARGET $id
+node "$BUILD_PATH"/node_modules/entu-ssg/src/build.js "$BUILD_PATH"/entu-ssg.yaml full
 printf '\n\n----------      Finished building      ----------\n\n'
 
-node ./helpers/reset_config_path_aliases.js
+node "$FETCH_PATH"/reset_config_path_aliases.js 
 
 
