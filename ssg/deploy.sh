423d5747f7b37f9273bcee99ec0ad2b14dce3891
marianntapfer <tapferm@gmail.com>
2021-03-12 17:28:00 +0200

    change back

--- ssg/deploy.sh
+++ ssg/deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 SECONDS=0
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DIR=$(dirname "$BASH_SOURCE")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 echo $PWD
 DOMAIN=$1
