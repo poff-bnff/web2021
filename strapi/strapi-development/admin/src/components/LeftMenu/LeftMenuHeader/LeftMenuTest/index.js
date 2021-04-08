e1ea0c47317b96d41c5131d05684e93293f3cab5
SiimS1 <siimsutt@hotmail.com>
2021-04-08 10:52:21 +0300

    extract fetchlogs, get tkn dynamic, retry if empty

--- strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/LeftMenuTest/index.js
+++ strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/LeftMenuTest/index.js
@@ -11,53 +11,19 @@ const LeftMenuTest = (props) => {
   useEffect(() => {
     console.log('LeftMenuTestIndex - useEffect');
 
-
-    // setTimeout(() => {
-    //   alert('hello')
-    // }, 1000);
-
-    async function fetchLogs() {
-
-      // let token = sessionStorage.getItem('jwtToken')
-
-      var myHeaders = new Headers();
-      myHeaders.append("Authorization", `Bearer `);
-
-      var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-      };
-      
-      let result = await fetch("http://localhost:1337/publisher/alllogs", requestOptions)
-        .then(response => response.text())
-        .then(result => result)
-        .catch(error => console.log('error', error));
-
-      result = JSON.parse(result)
-      console.log(result);
-      // console.log(result.end_time);
-
-      if (result.length < 1){
-        console.log('empty');
-        clearInterval(myVar);
-      }
-
-      if ((new Date(result[0].end_time)).getTime() - (new Date(result[0].start_time)).getTime() > 1000) {
-        clearInterval(myVar);
-        console.log(helloTest())
-        document.getElementById('x').innerHTML = result[0].end_time
-        strapi.notification.toggle({message: 'Your build of site ' + result[0].site + ' finished!', blockTransition: true, link: {url: `https://${result[0].site}`, label: 'See the result!'}})
-      }
-    }
-
     let myVar;
 
     function myFunction() {
       myVar = setInterval(fetchLogs, 3000);
     }
 
-    myFunction()
+    let result = myFunction()
+
+    if (result) {
+      clearInterval(myVar);
+    }
+
+
   }, []);
 
   return (
@@ -70,25 +36,66 @@ const LeftMenuTest = (props) => {
 export default LeftMenuTest;
 
 
-const setShownToUser = () => {
+
+async function fetchLogs() {
+  console.log('fetch');
+
+  // let token = sessionStorage.getItem('jwtToken')
 
   var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer ");
-myHeaders.append("Content-Type", "application/json");
+  const token = (sessionStorage.getItem('jwtToken')).replace(/"/g,'')
+  myHeaders.append("Authorization", `Bearer ${token}`);
 
-var raw = JSON.stringify({"shownToUser":"true"});
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+  let result = await fetch("http://localhost:1337/publisher/alllogs", requestOptions)
+    .then(response => response.text())
+    .then(result => result)
+    .catch(error => console.log('error', error));
 
-fetch("localhost:1337/publisher/alllogs", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  result = JSON.parse(result)
+
+  if (result.length < 1) {
+    return false
+  }
+
+  if ((new Date(result[0].end_time)).getTime() - (new Date(result[0].start_time)).getTime() > 1000) {
+    // clearInterval(myVar);
+    setShownToUser(result[0])
+    document.getElementById('x').innerHTML = result[0].end_time
+    strapi.notification.toggle({ message: 'Your build of site ' + result[0].site + ' finished!', blockTransition: true, link: { url: `https://${result[0].site}`, label: 'See the result!' } })
+  }
+}
+
+
+
+
+const setShownToUser = (log) => {
+  console.log(log);
+
+  const token = (sessionStorage.getItem('jwtToken')).replace(/"/g,'')
+  myHeaders.append("Authorization", `Bearer ${token}`);
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${token}`);
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({ shownToUser: true });
+
+  var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch(`http://localhost:1337/publisher/updatelog/${log.id}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
 
 }
