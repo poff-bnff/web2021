74642e33840748858570388b92dfeebc753ce7a5
SiimS1 <siimsutt@hotmail.com>
2021-04-14 11:50:18 +0300

    use adminpoffee

--- strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/index.js
+++ strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-const strapiHost = 'h'
+const strapiHost = 'https://admin.poff.ee'
 
 import Wrapper from './Wrapper';
 
