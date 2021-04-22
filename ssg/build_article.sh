5fad312cd8077c78e5fe79128c590b6f0f783ac9
Martin Pennaste <pennaste04@gmail.com>
2021-04-22 15:48:55 +0300

    Sünk

--- ssg/build_article.sh
+++ ssg/build_article.sh
@@ -41,8 +41,8 @@ node "$FETCH_PATH"/add_config_path_aliases.js display
 
 node "$BUILD_PATH"/node_modules/entu-ssg/src/build.js "$BUILD_PATH"/entu-ssg.yaml full
 
-echo "VÄIKE SÜNK $BUILD_PATH/build/$BUILDDIR/   $BUILD_PATH/../www/build.$DOMAIN"
-rsync "$BUILD_PATH"/build/"$BUILDDIR"/. "$BUILD_PATH"/../www/build."$DOMAIN"/
+echo "SÜNK $BUILD_PATH/build/$BUILDDIR/   $BUILD_PATH/../www/build.$DOMAIN"
+rsync -ra --progress "$BUILD_PATH"/build/"$BUILDDIR"/. "$BUILD_PATH"/../www/build."$DOMAIN"/
 
 printf '\n\n----------      Finished building      ----------\n\n'
 node "$FETCH_PATH"/reset_config_path_aliases.js
