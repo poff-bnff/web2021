e96078b6efe9dd73e6950e01297471446e93b574
SiimS1 <siimsutt@hotmail.com>
2021-05-03 20:35:05 +0300

    User lifecycles add ProfileFilled check

--- strapi/strapi-development/extensions/users-permissions/models/User.js
+++ strapi/strapi-development/extensions/users-permissions/models/User.js
@@ -4,4 +4,15 @@
  * Lifecycle callbacks for the `User` model.
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        beforeUpdate(params, data){
+            console.log(params, data);
+            if (data.firstName && data.lastName && data.gender && data.birthdate && data.phoneNr && data.address){
+                data.profileFilled = true
+            } else {
+                data.profileFilled = false
+            }
+        },
+    }
+}
