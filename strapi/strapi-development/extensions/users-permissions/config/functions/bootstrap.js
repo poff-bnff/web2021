7669be989f5d82dec8bdfbd6ec451f6c74d890f0
SiimS1 <siimsutt@hotmail.com>
2021-05-03 19:20:31 +0300

    Comment out auth0 - no icon causes error

--- strapi/strapi-development/extensions/users-permissions/config/functions/bootstrap.js
+++ strapi/strapi-development/extensions/users-permissions/config/functions/bootstrap.js
@@ -130,15 +130,15 @@ module.exports = async () => {
       callback: `${strapi.config.server.url}/auth/eventival/callback`,
       scope: ['openid', 'email', 'profile'],
     },
-    auth0: {
-      enabled: false,
-      icon: '',
-      key: '',
-      secret: '',
-      subdomain: 'my-tenant.eu',
-      callback: `${strapi.config.server.url}/auth/auth0/callback`,
-      scope: ['openid', 'email', 'profile'],
-    },
+    // auth0: {
+    //   enabled: false,
+    //   icon: '',
+    //   key: '',
+    //   secret: '',
+    //   subdomain: 'my-tenant.eu',
+    //   callback: `${strapi.config.server.url}/auth/auth0/callback`,
+    //   scope: ['openid', 'email', 'profile'],
+    // },
   };
   const prevGrantConfig = (await pluginStore.get({ key: 'grant' })) || {};
   // store grant auth config to db
