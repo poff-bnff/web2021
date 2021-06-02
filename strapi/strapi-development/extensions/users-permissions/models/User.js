080fa8efc4040364df6bfe2c12c93b81e901e34c
Liis <liis.kasper@gmail.com>
2021-06-02 10:45:39 +0300

    Add person to user if email match, if not, remove person

--- strapi/strapi-development/extensions/users-permissions/models/User.js
+++ strapi/strapi-development/extensions/users-permissions/models/User.js
@@ -9,6 +9,19 @@ module.exports = {
         beforeCreate (data){
             data.profileFilled = profileFilled(data)
         },
+        async beforeUpdate( params, data) {
+            // console.log('P', params, 'D', data)
+            data.people = []
+            let people = await strapi.query('person').find({ eMail_eq: data.email})
+            if (people.length > 0) {
+                let userAddedPersons = []
+                for(let person of people) {
+                    userAddedPersons.push({id: person.id})
+                }
+                data.people = userAddedPersons
+            }
+
+        },
         beforeDelete(params){
             delete params.user
         }
