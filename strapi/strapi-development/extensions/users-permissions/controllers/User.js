f2a89aaffd4e7cc6231352f25b1428a868a33e6a
SiimS1 <siimsutt@hotmail.com>
2021-06-09 18:36:04 +0300

    Create user import controller

--- strapi/strapi-development/extensions/users-permissions/controllers/User.js
+++ strapi/strapi-development/extensions/users-permissions/controllers/User.js
@@ -35,6 +35,10 @@ const resolveControllerMethod = method => ctx => {
   return callbackFn(ctx);
 };
 
+const formatError = error => [
+  { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
 module.exports = {
   create: resolveControllerMethod('create'),
   update: resolveControllerMethod('update'),
@@ -124,4 +128,81 @@ module.exports = {
 
     ctx.body = sanitizeUser(user);
   },
+
+  async import(ctx) {
+    // console.log(ctx.request.body)
+
+    const advanced = await strapi
+      .store({
+        environment: '',
+        type: 'plugin',
+        name: 'users-permissions',
+        key: 'advanced',
+      })
+      .get();
+
+    const { email, username, role, provider } = ctx.request.body;
+
+    if (!email) return ctx.badRequest('missing.email');
+    if (!username) return ctx.badRequest('missing.username');
+  //   if (!password) return ctx.badRequest('missing.password');
+
+    const userWithSameUsername = await strapi
+      .query('user', 'users-permissions')
+      .findOne({ username });
+
+    if (userWithSameUsername) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'Auth.form.error.username.taken',
+          message: 'Username already taken.',
+          field: ['username'],
+        })
+      );
+    }
+
+    if (advanced.unique_email) {
+      const userWithSameEmail = await strapi
+        .query('user', 'users-permissions')
+        .findOne({ email: email.toLowerCase() });
+
+      if (userWithSameEmail) {
+        return ctx.badRequest(
+          null,
+
+          formatError({
+            id: 'Auth.form.error.email.taken',
+            message: 'Email already taken.',
+            field: ['email'],
+          })
+        );
+      }
+    }
+
+    const user = {
+      ...ctx.request.body,
+      // provider: 'local',
+    };
+
+    user.email = user.email.toLowerCase();
+
+    if (!role) {
+      const defaultRole = await strapi
+        .query('role', 'users-permissions')
+        .findOne({ type: advanced.default_role }, []);
+
+      user.role = defaultRole.id;
+    }
+
+    try {
+      const data = await strapi.plugins['users-permissions'].services.user.add(user);
+
+      ctx.created(sanitizeUser(data));
+    } catch (error) {
+      ctx.badRequest(null, formatError(error));
+    }
+  }
+
+  
 };
