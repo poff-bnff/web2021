ee019ba17cff4d8130370a137e6d1b896303236c
SiimS1 <siimsutt@hotmail.com>
2021-09-17 10:41:01 +0000

    Server side changes (Martin)

--- strapi/strapi-development/config/plugins.js
+++ strapi/strapi-development/config/plugins.js
@@ -1,4 +1,3 @@
-
 module.exports = ({ env }) => ({
   email: {
     provider: 'mcmandrill',
@@ -7,6 +6,16 @@ module.exports = ({ env }) => ({
       mandrill_default_replyto: process.env.MandrillDefaultReplyToEmail,
       mandrill_default_from_email: process.env.MandrillDefaultFromEmail,
       mandrill_default_from_name: process.env.MandrillDefaultFromName
+    },
+  },
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: -1,
+    apolloServer: {
+      tracing: false,
     },
   },
 })
