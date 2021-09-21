b903779495fb28a0d5d0f991be6b2ba0729e126c
SiimS1 <siimsutt@hotmail.com>
2021-09-21 23:00:53 +0000

    LeftMenuHeader ENV (Martin)

--- strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/index.js
+++ strapi/strapi-development/admin/src/components/LeftMenu/LeftMenuHeader/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-const strapiHost = 'https://admin.poff.ee'
+const strapiHost = `${process.env['StrapiProtocol']}://${process.env['StrapiHost']}`
 // const strapiHost = 'http://localhost:1337'
 
 import Wrapper from './Wrapper';
