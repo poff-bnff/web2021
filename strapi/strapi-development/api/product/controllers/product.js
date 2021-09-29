f0079bfda3d8e7df0619f03e2a7e1cc81a5cf19c
Martin Pennaste <pennaste04@gmail.com>
2021-09-29 23:33:41 +0300

    Dynamic shop and product pages

--- strapi/strapi-development/api/product/controllers/product.js
+++ strapi/strapi-development/api/product/controllers/product.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async availability(ctx) {
+    const params = {
+      code_null: false,
+      reserved_to_null: true,
+      owner_null: true,
+      active: true,
+    }
+
+    let availabilityCheck = await strapi.services.product.find(params)
+
+    if (availabilityCheck) {
+      let productCatIds = availabilityCheck.map(a => {
+        return JSON.stringify({
+          id: a.product_category.id,
+          price: a.product_category.priceAtPeriod[0].price
+        })
+      })
+      let uniqueProductCatIds = [...new Set(productCatIds)]
+      return uniqueProductCatIds.map(p => JSON.parse(p))
+    } else {
+      return []
+    }
+  }
+
+};
