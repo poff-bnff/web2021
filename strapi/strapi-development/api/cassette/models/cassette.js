24a7b905775a4ac62c0b6d2a2897223b0a86b07e
SiimS1 <siimsutt@hotmail.com>
2021-12-28 08:39:11 +0000

    Remove media activate test from lifecycle(Liis)

--- strapi/strapi-development/api/cassette/models/cassette.js
+++ strapi/strapi-development/api/cassette/models/cassette.js
@@ -6,7 +6,6 @@
 
 const path = require('path')
 let helper_path = path.join(__dirname, '..', '..', '..', '/helpers/lifecycle_manager.js')
-let uploader_path = path.join(__dirname, '..', '..', '..', '/extensions/upload/services/Upload.js')
 
 const {
   slugify,
@@ -17,10 +16,6 @@ const {
   call_delete
 } = require(helper_path)
 
-
-const {
-  test
-} = require(uploader_path)
 
 /**
 const domains =
@@ -53,55 +48,39 @@ module.exports = {
         await call_delete(params, domains, model_name)
       }
 
-      if(data.media) {
-        let old_media = await strapi.query(model_name).findOne({ id: params.id})
-
-        // media j4lgimine siia. hetkel lihtsalt test
-        // console.log(JSON.stringify(old_media.media, 0, 2))
-        // console.log('B data ', JSON.stringify(data.media, 0, 2))
-
-        if (data.media.stills){
-          console.log(data.media.stills[0])
-
-          let test1 = await test(data.media.stills[0].id, model_name)
-          console.log({test1})
-        }
-
-      }
-
     },
     async afterUpdate(result, params, data) {
       // console.log('result- ', result , 'params- ', params, 'data- ', JSON.stringify(data, 0, 2))
 
-      // const domains = await get_domain(result) // hard coded if needed AS LIST!!!
-      // console.log('Create or update: ')
-      // if (data.skipbuild) return
-      // if (domains.length > 0) {
-      //   await modify_stapi_data(result, model_name)
-      // }
-      // await call_build(result, domains, model_name)
+      const domains = await get_domain(result) // hard coded if needed AS LIST!!!
+      console.log('Create or update: ')
+      if (data.skipbuild) return
+      if (domains.length > 0) {
+        await modify_stapi_data(result, model_name)
+      }
+      await call_build(result, domains, model_name)
     },
 async beforeDelete(params) {
-      // const ids = params._where?.[0].id_in || [params.id]
-      // const updatedIds = await Promise.all(ids.map(async id => {
-      //   const result = await strapi.query(model_name).findOne({ id })
-      //   if (result){
-      //   const updateDeleteUser = {
-      //     updated_by: params.user,
-      //     skipbuild: true
-      //   }
-      //   await strapi.query(model_name).update({ id: result.id }, updateDeleteUser)
-      //   return id
-      //   }
-      // }))
-      // delete params.user
+      const ids = params._where?.[0].id_in || [params.id]
+      const updatedIds = await Promise.all(ids.map(async id => {
+        const result = await strapi.query(model_name).findOne({ id })
+        if (result){
+        const updateDeleteUser = {
+          updated_by: params.user,
+          skipbuild: true
+        }
+        await strapi.query(model_name).update({ id: result.id }, updateDeleteUser)
+        return id
+        }
+      }))
+      delete params.user
     },
     async afterDelete(result, params) {
-      // // console.log('\nR', result, '\nparams', params)
-      // const domains = await get_domain(result) // hard coded if needed AS LIST!!!
+      // console.log('\nR', result, '\nparams', params)
+      const domains = await get_domain(result) // hard coded if needed AS LIST!!!
 
-      // console.log('Delete: ')
-      // await call_delete(result, domains, model_name)
+      console.log('Delete: ')
+      await call_delete(result, domains, model_name)
     }
   }
 };
