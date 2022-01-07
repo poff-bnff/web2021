4cdfe72bad29fae2d323133907939af84d81dbee
Martin Pennaste <pennaste04@gmail.com>
2022-01-07 03:28:18 +0200

    image_prioritizer to article fetch

--- ssg/helpers/fetch_articles_from_yaml.js
+++ ssg/helpers/fetch_articles_from_yaml.js
@@ -2,29 +2,33 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 const path = require('path')
 const rueten = require('./rueten.js')
-const {fetchModel} = require('./b_fetch.js')
+const { fetchModel } = require('./b_fetch.js')
 const replaceLinks = require('./replace_links.js')
+const prioritizeImages = require(path.join(__dirname, 'image_prioritizer.js'))
 
 const { timer } = require("./timer")
 timer.start(__filename)
 
-const rootDir =  path.join(__dirname, '..')
+const rootDir = path.join(__dirname, '..')
 const domainSpecificsPath = path.join(rootDir, 'domain_specifics.yaml')
 
 const DOMAIN_SPECIFICS = yaml.load(fs.readFileSync(domainSpecificsPath, 'utf8'))
 
-const sourceDir =  path.join(rootDir, 'source')
-const fetchDir =  path.join(sourceDir, '_fetchdir')
+const imageOrder = DOMAIN_SPECIFICS.articleListViewImagePriority
+const imageOrderDefaults = DOMAIN_SPECIFICS.articleListViewImagePriorityDefaults
+
+const sourceDir = path.join(rootDir, 'source')
+const fetchDir = path.join(sourceDir, '_fetchdir')
 const strapiDataDirPath = path.join(sourceDir, '_domainStrapidata')
 
 const params = process.argv.slice(2)
 const build_type = params[0]
 const only_build_home = params[1] === 'HOME' ? true : false
 
-const DOMAIN = process.env['DOMAIN'] || 'poff.ee'
+const DOMAIN = process.env['DOMAIN'] || 'kumu.poff.ee'
 
 const addConfigPathAliases = require(path.join(__dirname, 'add_config_path_aliases.js'))
-if(build_type === 'target') {
+if (build_type === 'target') {
     if (only_build_home) {
         addConfigPathAliases(['/home'])
     } else {
@@ -38,52 +42,52 @@ const strapiDataArticlesPath = path.join(strapiDataDirPath, `${modelName}.yaml`)
 const STRAPIDATA_ARTICLES = yaml.load(fs.readFileSync(strapiDataArticlesPath, 'utf8'))
 
 const minimodel = {
-        'article_types': {
-            model_name: 'ArticleType'
-        },
-        'tag_premiere_types': {
-            model_name: 'TagPremiereType'
-        },
-        'programmes': {
-            model_name: 'Programme',
-            expand: {
-                'festival_editions': {
-                    model_name: 'FestivalEdition',
-                    expand: {
-                        'festival': {
-                            model_name: 'Festival'
-                        }
-                    },
+    'article_types': {
+        model_name: 'ArticleType'
+    },
+    'tag_premiere_types': {
+        model_name: 'TagPremiereType'
+    },
+    'programmes': {
+        model_name: 'Programme',
+        expand: {
+            'festival_editions': {
+                model_name: 'FestivalEdition',
+                expand: {
+                    'festival': {
+                        model_name: 'Festival'
+                    }
                 },
-                'presenters': {
-                    model_name: 'Organisation'
-                },
-                'domains': {
-                    model_name: 'Domain'
-                },
-                'presentedBy': {
-                    model_name: 'PresentedBy',
-                    expand: {
-                        'organisations': {
-                            model_name: 'Organisation'
-                        }
+            },
+            'presenters': {
+                model_name: 'Organisation'
+            },
+            'domains': {
+                model_name: 'Domain'
+            },
+            'presentedBy': {
+                model_name: 'PresentedBy',
+                expand: {
+                    'organisations': {
+                        model_name: 'Organisation'
                     }
                 }
-            },
+            }
         },
-        'tag_genres': {
-            model_name: 'TagGenre'
-        },
-        'tag_keywords': {
-            model_name: 'TagKeyword'
-        },
-        'web_authors': {
-            model_name: 'WebAuthor'
-        },
-        'organisations': {
-            model_name: 'Organisation'
-        }
+    },
+    'tag_genres': {
+        model_name: 'TagGenre'
+    },
+    'tag_keywords': {
+        model_name: 'TagKeyword'
+    },
+    'web_authors': {
+        model_name: 'WebAuthor'
+    },
+    'organisations': {
+        model_name: 'Organisation'
     }
+}
 
 STRAPIDATA_ARTICLE = fetchModel(STRAPIDATA_ARTICLES, minimodel)
 
@@ -133,6 +137,12 @@ for (const lang of allLanguages) {
                 element.lead = replaceLinks(element.lead, stagingURL, pageURL);
 
             }
+
+            // Article list view get priority picture format
+            const primaryImage = prioritizeImages(element, imageOrder, imageOrderDefaults)
+            if (primaryImage) { element.primaryImage = primaryImage }
+            // Delete excess media
+            delete element.media
 
             allData.push(element)
             element.data = dataFrom
