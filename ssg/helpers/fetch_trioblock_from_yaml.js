4fecf066270d3ea75b9e07a1d122b173917028b9
Martin Pennaste <pennaste04@gmail.com>
2022-01-12 23:39:40 +0200

    Trioblock picture formats prioritize

--- ssg/helpers/fetch_trioblock_from_yaml.js
+++ ssg/helpers/fetch_trioblock_from_yaml.js
@@ -2,11 +2,19 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 const path = require('path')
 const rueten = require('./rueten.js')
+const prioritizeImages = require(path.join(__dirname, 'image_prioritizer.js'))
 
-const sourceDir =  path.join(__dirname, '..', 'source')
+const rootDir = path.join(__dirname, '..')
+
+const sourceDir =  path.join(rootDir, 'source')
 const fetchDir =  path.join(sourceDir, '_fetchdir')
 const strapiDataDirPath = path.join(sourceDir, '_domainStrapidata')
-const DOMAIN = process.env['DOMAIN'] || 'poff.ee'
+const domainSpecificsPath = path.join(rootDir, 'domain_specifics.yaml')
+const DOMAIN_SPECIFICS = yaml.load(fs.readFileSync(domainSpecificsPath, 'utf8'))
+const imageOrder = DOMAIN_SPECIFICS.trioblockImagePriority
+const imageOrderDefaults = DOMAIN_SPECIFICS.trioblockImagePriorityDefaults
+
+const DOMAIN = process.env['DOMAIN'] || 'justfilm.ee'
 
 const params = process.argv.slice(2)
 const build_type = params[0]
@@ -67,19 +75,38 @@ for (const lang of languages) {
                 continue
             }
 
-            if (copyData[key][key2][`${articleMapping[DOMAIN]}_article`] !== undefined) {
+            let trioArticle = copyData[key][key2][`${articleMapping[DOMAIN]}_article`]
+            let trioBlock = copyData[key][key2]
+
+            // PROCESS IMAGES
+            // If any images, construct temporary media component for image prioritizer
+            if (trioBlock.image) {
+                trioBlock.media = {}
+                trioBlock.media.image = [trioBlock.image]
+                // Delete original image data, as it being no longer needed
+                delete trioBlock.image
+            }
+
+            // Prioritize images
+            const primaryImage = prioritizeImages(trioBlock, imageOrder, imageOrderDefaults)
+            if (primaryImage) { trioBlock.heroImage = primaryImage }
+
+            // Delete temporary media component
+            delete trioBlock.media
+
+            if (trioArticle !== undefined) {
                 buffer.push({
-                    'block': copyData[key][key2],
-                    'article': copyData[key][key2][`${articleMapping[DOMAIN]}_article`]
+                    'block': trioBlock,
+                    'article': trioArticle
                 })
-                delete copyData[key][key2][`${articleMapping[DOMAIN]}_article`]
-            } else if (copyData[key][key2] !== undefined) {
+                delete trioArticle
+            } else if (trioBlock !== undefined) {
                 buffer.push({
-                    'block': copyData[key][key2],
+                    'block': trioBlock,
                 })
             }
 
-            delete copyData[key][key2]
+            delete trioBlock
         }
     }
     const outFile = path.join(fetchDir, `articletrioblock.${lang}.yaml`)
