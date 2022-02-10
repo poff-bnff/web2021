44d1f94258e80359952b144c2f3a00d0c305e7a2
Liis <liis.kasper@gmail.com>
2022-02-10 12:48:11 +0200

    add user pic resize options to domain_specifics

--- strapi/strapi-development/extensions/upload/services/image-manipulation.js
+++ strapi/strapi-development/extensions/upload/services/image-manipulation.js
@@ -3,6 +3,9 @@
  * Image manipulation functions
  */
 const sharp = require('sharp');
+const path = require('path')
+const yaml = require('js-yaml')
+const fs = require('fs')
 
 const { bytesToKbytes } = require('../utils/file');
 
@@ -16,107 +19,11 @@ const getDimensions = buffer =>
     .then(({ width = null, height = null }) => ({ width, height }))
     .catch(() => ({})); // ignore errors
 
-const resize_options = [
-  {'F_': [{
-    sufiks: '_big_16_9',
-    width: 1920,
-    height: 1080,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_med_16_9',
-    width: 1280,
-    height: 720,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_small_16_9',
-    width: 640,
-    height: 360,
-    fit: 'cover'
-  }]},
-  {'R_': [{
-    sufiks: '_big_sq',
-    width: 900,
-    height: 900,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_med_sq',
-    width: 500,
-    height: 500,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_small_sq',
-    width: 300,
-    height: 300,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_thumb_sq',
-    width: 100,
-    height: 100,
-    fit: 'cover'
-  }]},
-  {'P_': [{
-    sufiks: '_big_p',
-    width: 555,
-    height: 800,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_small_p',
-    width: 300,
-    height: 207,
-    fit: 'cover'
-  }]},
-  {'NO': [{
-    sufiks: '_big',
-    width: 1920,
-    fit: 'inside'
-  },
-  {
-    sufiks: '_med',
-    width: 1280,
-    fit: 'inside'
-  },
-  {
-    sufiks: '_small',
-    width: 640,
-    fit: 'inside'
-  }]},
-  {'U_': [{
-    sufiks: '_big_sq',
-    width: 900,
-    height: 900,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_med_sq',
-    width: 500,
-    height: 500,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_small_sq',
-    width: 300,
-    height: 300,
-    fit: 'cover'
-  },
-  {
-    sufiks: '_thumb_sq',
-    width: 100,
-    height: 100,
-    fit: 'cover'
-  }]}
-]
+const domainSpecificsPath = path.join(__dirname, '..', '..', '..', '..', '..', 'ssg' ,'domain_specifics.yaml')
+const DOMAIN_SPECIFICS = yaml.load(fs.readFileSync(domainSpecificsPath, 'utf8'))
+const resize_options = DOMAIN_SPECIFICS.libraryImageResizeOptions
 
-const THUMBNAIL_RESIZE_OPTIONS = {
-  width: 245,
-  height: 156,
-  fit: 'inside',
-};
+const THUMBNAIL_RESIZE_OPTIONS = DOMAIN_SPECIFICS.libraryImageThumbnailResizeOption
 
 async function findOptions(prefix){
   let options = null 
