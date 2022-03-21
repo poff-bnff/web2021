b491890c7b417284b4b3a32179f949d0da46604f
Martin Pennaste <pennaste04@gmail.com>
2022-03-21 17:51:31 +0200

    User lifecycle for course products

--- strapi/strapi-development/extensions/users-permissions/models/User.js
+++ strapi/strapi-development/extensions/users-permissions/models/User.js
@@ -36,9 +36,6 @@ module.exports = {
 
       // Check if my_products not undefined (happens when updating moodle_id)
       if (my_products) {
-
-
-
         let addedProductsArray = [...new Set(my_products.filter(x => !my_products_before_update.includes(x)))]
         let removedProductsArray = [...new Set(my_products_before_update.filter(x => !my_products.includes(x)))]
         let allAffectedProducts = [...addedProductsArray].concat(removedProductsArray)
@@ -47,22 +44,24 @@ module.exports = {
         if (removedProductsArray.length) { console.log('removedProduct(s)', removedProductsArray); }
         if (allAffectedProducts.length) { console.log('allAffectedProducts', allAffectedProducts); }
 
-        const fullProductInfos = await strapi.query('product').find({ 'id_in': allAffectedProducts }, ['product_category', 'product_category.product_types']);
+        const fullProductInfos = await strapi.query('product').find({ 'id_in': allAffectedProducts }, ['product_category', 'product_category.product_types', 'product_category.courses']);
         const sanitizedFullProductInfos = sanitizeEntity(fullProductInfos, {
           model: strapi.query('product').model,
         });
-        // Filter products with category type course and which category type moodle_id present
-        let courseProducts = sanitizedFullProductInfos.filter(p => p?.product_category?.product_types?.map(t => t.id).includes(2) && p?.product_category?.moodle_id);
+        // Filter products with category type course (ID 2)
+        let productsCategories = sanitizedFullProductInfos.filter(p => p?.product_category?.product_types?.map(t => t.id).includes(2));
 
-        if (courseProducts.length) {
+        if (productsCategories.length) {
           if (result?.user_profile?.firstName && result?.user_profile?.lastName) {
             const userEmail = result.email
             const userFirstName = result?.user_profile?.firstName
             const userLastName = result?.user_profile?.lastName
+
+            let userMoodleId
             // Check if moodle_id already under Strapi user
             if (!result.moodle_id) {
               const getMoodleUserInfo = await getUser(userEmail)
-              let userMoodleId = getMoodleUserInfo?.users?.[0]?.id
+              userMoodleId = getMoodleUserInfo?.users?.[0]?.id
               // If already, update user
               if (userMoodleId) {
                 console.log('Moodle user exists with ID:', userMoodleId, 'Updating Strapi user with Moodle ID');
@@ -76,31 +75,49 @@ module.exports = {
                 userMoodleId = createMoodleUser[0].id
               }
 
-              // By now Strapi user should have moodle ID
-              if (userMoodleId) {
-                // Enrol user to courses that were added
-                if (addedProductsArray.length) {
-                  for (let i = 0; i < addedProductsArray.length; i++) {
-                    const courseId = courseProducts.filter(p => p.id === addedProductsArray[i])[0].product_category?.moodle_id;
-                    const enrolMoodleUser = await enrolUser(userMoodleId, courseId, 5)
-                    console.log('Moodle user', userMoodleId, 'enrolled to course ', courseId);
-                  }
+            } else { userMoodleId = result.moodle_id }
+
+            // By now Strapi user should have moodle ID to enrol Moodle courses
+            if (userMoodleId) {
+              // Enrol user to courses that were added
+              if (addedProductsArray.length) {
+                const courseIds = []
+                addedProductsArray.map(ap => {
+                  productsCategories.filter(p => p.id === ap)
+                    .map(p => p?.product_category?.courses?.filter(c => c.moodle_id).map(c => courseIds.push(c.moodle_id)))
+                })
+
+                const uniqueCourseIds = [... new Set(courseIds)]
+                for (let i = 0; i < uniqueCourseIds.length; i++) {
+                  const courseId = uniqueCourseIds[i];
+                  console.log('courseId', courseId);
+                  const enrolMoodleUser = await enrolUser(userMoodleId, courseId, 5)
+                  console.log('Moodle user', userMoodleId, 'enrolled to course ', courseId);
                 }
-                // Unenrol user from courses that were removed
-                if (removedProductsArray.length) {
-                  for (let i = 0; i < removedProductsArray.length; i++) {
-                    const courseId = courseProducts.filter(p => p.id === removedProductsArray[i])[0].product_category?.moodle_id;
-                    const unEnrolMoodleUser = await enrolUser(userMoodleId, courseId, 5, true)
-                    console.log('Moodle user', userMoodleId, 'unenrolled from course ', courseId);
-                  }
+              }
+
+              // Unenrol user from courses that were removed
+              if (removedProductsArray.length) {
+                const courseIds = []
+                removedProductsArray.map(rp => {
+                  productsCategories.filter(p => p.id === rp)
+                    .map(p => p?.product_category?.courses?.filter(c => c.moodle_id).map(c => courseIds.push(c.moodle_id)))
+                })
+
+                const uniqueCourseIds = [... new Set(courseIds)]
+                for (let i = 0; i < uniqueCourseIds.length; i++) {
+                  const courseId = uniqueCourseIds[i];
+                  console.log('courseId', courseId);
+                  const enrolMoodleUser = await enrolUser(userMoodleId, courseId, 5, true)
+                  console.log('Moodle user', userMoodleId, 'unenrolled from course ', courseId);
                 }
               }
             }
           } else {
-          console.log('Profile not filled');
+            console.log('Profile not filled');
           }
         } else {
-          console.log('No course products changed');
+          console.log('User, but no course products changed');
         }
       }
     },
