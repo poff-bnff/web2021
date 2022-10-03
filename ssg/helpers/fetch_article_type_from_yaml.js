087c3f34f63ca97dddbb99a357a6d45c4c9f3bd7
Jaan Leppik <60266520+jaanleppik@users.noreply.github.com>
2022-10-03 20:37:21 +0300

    votemo poff

--- ssg/helpers/fetch_article_type_from_yaml.js
+++ ssg/helpers/fetch_article_type_from_yaml.js
@@ -222,7 +222,7 @@ for (const lang of languages) {
                         //     element.aliases = [`${votemoAliasLangPrefix}votemo`]
                         // }
                         // 2022 adding votemo (artikli slug votemo) for TARTUFF
-                        if (DOMAIN === 'tartuff.ee' && element[key] === 'votemo') {
+                        if (DOMAIN === 'poff.ee' && element[key] === 'votemo') {
                             let votemooAliasLangPrefix = lang === 'et' ? '' : `${lang}/`
                             element.aliases = [`${votemooAliasLangPrefix}votemo`]
                         }
