a6258561006afa787be595972d29e3f73a6f6cee
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-05-23 12:18:50 +0300

    time yaml operations

--- ssg/helpers/fetch_cassettes_from_yaml.js
+++ ssg/helpers/fetch_cassettes_from_yaml.js
@@ -38,23 +38,27 @@ timer.log(__filename, `Merging Cassette_updates.yaml with Cassette.yaml`)
 const strapiDataCassettePath = path.join(strapiDataDirPath, 'Cassette.yaml')
 const strapiDataCassetteUpdatesPath = path.join(strapiDataDirPath, 'Cassette_updates.yaml')
 if (fs.existsSync(strapiDataCassetteUpdatesPath)) {
-    console.log('Cassette_updates.yaml exists, merging with Cassette.yaml')
+    timer.log(__filename, 'Loading Cassette.yaml')
     const base = yaml.load(fs.readFileSync(strapiDataCassettePath, 'utf8'))
     // convert array to object, so that we can use cassette.id as key
     .reduce((obj, item) => {
         obj[item.id] = item
         return obj
     }, {})
+    timer.log(__filename, 'Loading Cassette_updates.yaml')
     let updates = yaml.load(fs.readFileSync(strapiDataCassetteUpdatesPath, 'utf8'))
     // do the same for updates
     .reduce((obj, item) => {
         obj[item.id] = item
         return obj
     }, {})
+    timer.log(__filename, 'Merging Cassette.yaml and Cassette_updates.yaml')
     // merge base and updates and convert back to array and write to Cassette.yaml
     let merged = Object.values(Object.assign({}, base, updates))
+    timer.log(__filename, 'Writing Cassette.yaml')
     fs.writeFileSync(strapiDataCassettePath, yaml.dump(merged))
     // delete Cassette_updates.yaml
+    timer.log(__filename, 'Deleting Cassette_updates.yaml')
     fs.unlinkSync(strapiDataCassetteUpdatesPath)
 }
 timer.log(__filename, 'Loading Cassette.yaml')
