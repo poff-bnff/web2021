8b639c6a72c6caa238b27c96cef53f0460fe7b28
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-09-17 06:53:55 +0300

    render favs on single film cassette page

---
+++ ssg/source/films/script.js
@@ -0,0 +1,68 @@
+const nslButton = document.getElementsByClassName('notshortlisted')[0]
+const slButton = document.getElementsByClassName('isshortlisted')[0]
+const currentFilmId = document.getElementById('film_id').value
+
+if (getUser()) {
+    const userFilms = getUser().My?.films?.map(f=>f.id) || []
+    const currentFilmIsFavourite = userFilms.includes(parseInt(currentFilmId))
+
+    if (currentFilmIsFavourite) {
+        nslButton.style.display = 'none'
+        slButton.style.display = ''
+    } else {
+        nslButton.style.display = ''
+        slButton.style.display = 'none'
+    }
+
+    nslButton.addEventListener('click', e => {
+        toggleFavouriteFilm('set')
+    })
+    slButton.addEventListener('click', e => {
+        toggleFavouriteFilm('unset')
+    })
+}
+
+function toggleFavouriteFilm(action) {
+    const nslButton = document.getElementById(`${favId}_not_shortlisted`)
+    const slButton = document.getElementById(`${favId}_is_shortlisted`)
+
+    const pushedButton = action === 'set' ? nslButton : slButton
+    const pushedButtonInnerHTMLBeforeClick = pushedButton.innerHTML
+
+    pushedButton.innerHTML = `<i class="fa fa-spinner fa-spin"></i>`
+    pushedButton.disabled = true
+
+    var myHeaders = new Headers()
+    myHeaders.append("Authorization", 'Bearer ' + localStorage.getItem('ID_TOKEN'))
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        redirect: 'follow',
+        body: currentFilmId
+    };
+
+    // `${strapiDomain}/users/favorites/`
+    fetch(`${huntAuthDomain}/api/my/film`, requestOptions).then(function (response) {
+        if (response.ok) {
+            return response.json()
+        }
+        return Promise.reject(response)
+    }).then(function (data) {
+        if (action === 'set') {
+            nslButton.style.display = 'none'
+            slButton.style.display = ''
+        } else {
+            nslButton.style.display = ''
+            slButton.style.display = 'none'
+        }
+        pushedButton.innerHTML = pushedButtonInnerHTMLBeforeClick
+        pushedButton.disabled = false
+        const webUser = getUser()
+        webUser.My = data
+        setUser(webUser)
+    }).catch(function (error) {
+        console.warn(error);
+        pushedButton.innerHTML = 'Tekkis viga!'
+    });
+}
