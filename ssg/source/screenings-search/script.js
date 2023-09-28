dd5a59bd9a0dbf256fd55a64a8c33ec637825641
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-09-28 16:28:37 +0300

    favs to screenings-search

--- ssg/source/screenings-search/script.js
+++ ssg/source/screenings-search/script.js
@@ -346,4 +346,47 @@ function execute_filters() {
     return filtered
 }
 
+document.onreadystatechange = () => {
+    console.log(`document.readyState: ${document.readyState}`)
+    if (document.readyState === 'complete') {
+        setupScreeningFavoriteButtons()
+    }
+
+    function setupScreeningFavoriteButtons () {
+        const nslButtons = Array.from(document.getElementsByClassName('notmyscreening'))
+        const slButtons = Array.from(document.getElementsByClassName('ismyscreening'))
+        const currentScreeningIDs = Array.from(document.getElementById('screening_ids').value.split(','))
+            .map(e => parseInt(e))
+
+        if (getUser()) {
+            const myScreenings = reloadUserScreenings()
+            //- console.log({myScreenings, currentScreeningIDs})
+
+            // unhide all fav buttons for currently favorited screenings
+            currentScreeningIDs.filter(id => myScreenings.includes(id))
+                .forEach(id => {
+                    document.getElementById(`s_${id}_is_fav`).style.display = ''
+                    document.getElementById(`s_${id}_is_not_fav`).style.display = 'none'
+                })
+
+            // unhide all no-fav buttons for currently unfavorited screenings
+            currentScreeningIDs.filter(id => !myScreenings.includes(id))
+                .forEach(id => {
+                    document.getElementById(`s_${id}_is_fav`).style.display = 'none'
+                    document.getElementById(`s_${id}_is_not_fav`).style.display = ''
+                })
+
+            // add event listeners to all fav buttons
+            nslButtons.forEach(b => b.addEventListener('click', e => {
+                let scrId = parseInt(b.id.split('_')[1])
+                toggleFavouriteScreening('set', scrId)
+            }))
+            slButtons.forEach(b => b.addEventListener('click', e => {
+                let scrId = parseInt(b.id.split('_')[1])
+                toggleFavouriteScreening('unset', scrId)
+            }))
+        }
+    }
+
+}
 
