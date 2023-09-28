7d1063e43ca9f6fc1edd573e24fe7db54ffff63f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-09-28 11:29:40 +0300

    Update devs_build.sh

--- ssg/devs_build.sh
+++ ssg/devs_build.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
 
 git pull
-node ./node_modules/entu-ssg/src/build.js ./entu-ssg-devs.yaml full
+DOMAIN=poff.ee node ./node_modules/entu-ssg/src/build.js ./entu-ssg-devs.yaml full
 
 rsync -rav ./build/poff/. ./build/web/
 
