2439a7a9c58b5a7081808fd5632e3fd128a91239
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-10-28 21:34:06 +0300

    Update fetch_person_from_yaml.js

--- ssg/helpers/fetch_person_from_yaml.js
+++ ssg/helpers/fetch_person_from_yaml.js
@@ -113,6 +113,8 @@ if (DOMAIN !== 'industry.poff.ee') {
 
     const activeEditions = DOMAIN_SPECIFICS.active_editions['industry.poff.ee']
     console.log('activeEditions', activeEditions)
+    const personsWithEditions = STRAPIDATA_ALL_PERSONS.filter(p => p.festival_editions && p.festival_editions.length)
+    console.log('personsWithEditions', personsWithEditions.length)
     const activePersons = STRAPIDATA_ALL_PERSONS
         .filter(p => {
             const feIds = (p.festival_editions || []).map(fe => fe.id) || []
