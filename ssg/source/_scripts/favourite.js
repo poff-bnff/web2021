824283eec714fc24340a39165742d754cd5253f2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-10-30 21:29:02 +0200

    try to solve #678, again

--- ssg/source/_scripts/favourite.js
+++ ssg/source/_scripts/favourite.js
@@ -124,78 +124,86 @@ function toggleFavouriteFilm(action, favId) {
 }
 
 function setupScreeningFavoriteButtons() {
-    const nslButtons = Array.from(document.getElementsByClassName('notmyscreening'))
-    const slButtons = Array.from(document.getElementsByClassName('ismyscreening'))
-    const currentScreeningIDs = Array.from(document.getElementById('screening_ids').value.split(','))
-        .map(e => parseInt(e))
+    try {
+        const nslButtons = Array.from(document.getElementsByClassName('notmyscreening'))
+        const slButtons = Array.from(document.getElementsByClassName('ismyscreening'))
+        const currentScreeningIDs = Array.from(document.getElementById('screening_ids').value.split(','))
+            .map(e => parseInt(e))
 
-    if (getUser()) {
-        reloadUserScreenings()
+        if (getUser()) {
+            reloadUserScreenings()
 
-        // unhide all fav buttons for currently favorited screenings
-        currentScreeningIDs.filter(id => userScreenings.includes(id))
-            .forEach(id => {
-                document.getElementById(`s_${id}_is_fav`).style.display = ''
-                document.getElementById(`s_${id}_is_not_fav`).style.display = 'none'
-            })
+            // unhide all fav buttons for currently favorited screenings
+            currentScreeningIDs.filter(id => userScreenings.includes(id))
+                .forEach(id => {
+                    document.getElementById(`s_${id}_is_fav`).style.display = ''
+                    document.getElementById(`s_${id}_is_not_fav`).style.display = 'none'
+                })
 
-        // unhide all no-fav buttons for currently unfavorited screenings
-        currentScreeningIDs.filter(id => !userScreenings.includes(id))
-            .forEach(id => {
-                document.getElementById(`s_${id}_is_fav`).style.display = 'none'
-                document.getElementById(`s_${id}_is_not_fav`).style.display = ''
-            })
+            // unhide all no-fav buttons for currently unfavorited screenings
+            currentScreeningIDs.filter(id => !userScreenings.includes(id))
+                .forEach(id => {
+                    document.getElementById(`s_${id}_is_fav`).style.display = 'none'
+                    document.getElementById(`s_${id}_is_not_fav`).style.display = ''
+                })
 
-        // add event listeners to all fav buttons
-        nslButtons.forEach(b => b.addEventListener('click', e => {
-            let scrId = parseInt(b.id.split('_')[1])
-            toggleFavouriteScreening('set', scrId)
-        }))
-        slButtons.forEach(b => b.addEventListener('click', e => {
-            let scrId = parseInt(b.id.split('_')[1])
-            toggleFavouriteScreening('unset', scrId)
-        }))
+            // add event listeners to all fav buttons
+            nslButtons.forEach(b => b.addEventListener('click', e => {
+                let scrId = parseInt(b.id.split('_')[1])
+                toggleFavouriteScreening('set', scrId)
+            }))
+            slButtons.forEach(b => b.addEventListener('click', e => {
+                let scrId = parseInt(b.id.split('_')[1])
+                toggleFavouriteScreening('unset', scrId)
+            }))
+        }
+    } catch (error) {
+        console.log(error)
     }
 }
 
 function setupFilmFavoriteButtons() {
-    const nslButton = document.getElementsByClassName('notshortlisted')[0]
-    const slButton = document.getElementsByClassName('isshortlisted')[0]
-    const cassetteIdElement = document.getElementById('film_id') || null
-    const cassetteId = cassetteIdElement?.value || null
-    const currentFilmID = parseInt(cassetteId) || null
+    try {
+        const nslButton = document.getElementsByClassName('notshortlisted')[0]
+        const slButton = document.getElementsByClassName('isshortlisted')[0]
+        const cassetteIdElement = document.getElementById('film_id') || null
+        const cassetteId = cassetteIdElement?.value || null
+        const currentFilmID = parseInt(cassetteId) || null
 
-    if (getUser()) {
-        reloadUserFilms()
-        if (nslButton && slButton && currentFilmID) {
-            const currentFilmIsFavourite = userFilms.includes(currentFilmID)
+        if (getUser()) {
+            reloadUserFilms()
+            if (nslButton && slButton && currentFilmID) {
+                const currentFilmIsFavourite = userFilms.includes(currentFilmID)
 
-            if (currentFilmIsFavourite) {
-                nslButton.style.display = 'none'
-                slButton.style.display = ''
-            } else {
-                nslButton.style.display = ''
-                slButton.style.display = 'none'
+                if (currentFilmIsFavourite) {
+                    nslButton.style.display = 'none'
+                    slButton.style.display = ''
+                } else {
+                    nslButton.style.display = ''
+                    slButton.style.display = 'none'
+                }
+
+                nslButton.addEventListener('click', e => {
+                    toggleFavouriteFilm('set', currentFilmID)
+                })
+                slButton.addEventListener('click', e => {
+                    toggleFavouriteFilm('unset', currentFilmID)
+                })
             }
-
-            nslButton.addEventListener('click', e => {
-                toggleFavouriteFilm('set', currentFilmID)
-            })
-            slButton.addEventListener('click', e => {
-                toggleFavouriteFilm('unset', currentFilmID)
-            })
+            reloadUserScreenings()
+            const currentScreeningIDs = Array.from(document.getElementsByClassName('card_screening'))
+                .map(e => parseInt(e.id.slice(1)))
+            // unhide all fav buttons for currently favorited screenings
+            currentScreeningIDs.filter(id => userScreenings.includes(id))
+                .forEach(id => {
+                    try {
+                        document.getElementById(`s_${id}_is_fav`).style.display = ''
+                        document.getElementById(`s_${id}_is_not_fav`).style.display = 'none'
+                    } catch (err) { null }
+                })
         }
-        reloadUserScreenings()
-        const currentScreeningIDs = Array.from(document.getElementsByClassName('card_screening'))
-            .map(e => parseInt(e.id.slice(1)))
-        // unhide all fav buttons for currently favorited screenings
-        currentScreeningIDs.filter(id => userScreenings.includes(id))
-            .forEach(id => {
-                try {
-                    document.getElementById(`s_${id}_is_fav`).style.display = ''
-                    document.getElementById(`s_${id}_is_not_fav`).style.display = 'none'
-                } catch (err) { null }
-            })
+    } catch (error) {
+        console.log(error)
     }
 }
 
