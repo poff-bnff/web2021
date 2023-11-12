52bebd3baf16eae22f2f11fa539d932cdde32033
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-11-12 23:27:06 +0200

    cleanup person lifecycles

--- strapi/strapi-development/api/person/models/person.js
+++ strapi/strapi-development/api/person/models/person.js
@@ -138,43 +138,18 @@ const domains = ['FULL_BUILD'] // hard coded if needed AS LIST!!!
 module.exports = {
     lifecycles: {
         async afterCreate(result, data) {
-            await call_update(result, model_name)
         },
+
         async beforeUpdate(params, data) {
-
-            if (data.published_at === null) { // if strapi publish system goes live
-                console.log('Draft! Delete: ')
-                await call_delete(params, domains, model_name)
-            }
         },
+
         async afterUpdate(result, params, data) {
-            console.log('Create or update: ')
-            if (data.skipbuild) return
-            if (domains.length > 0) {
-                await modify_stapi_data(result, model_name)
-            }
-            await call_build(result, domains, model_name)
         },
-        async beforeDelete(params) {
-      const ids = params._where?.[0].id_in || [params.id]
-      const updatedIds = await Promise.all(ids.map(async id => {
-        const result = await strapi.query(model_name).findOne({ id })
-        if (result){
-        const updateDeleteUser = {
-          updated_by: params.user,
-          skipbuild: true
-        }
-        await strapi.query(model_name).update({ id: result.id }, updateDeleteUser)
-        return id
-        }
-      }))
-      delete params.user
-    },
-        async afterDelete(result, params) {
-            // console.log('\nR', result, '\nparams', params)
 
-            console.log('Delete: ')
-            await call_delete(result, domains, model_name)
+        async beforeDelete(params) {
+        },
+
+        async afterDelete(result, params) {
         }
     }
 };
