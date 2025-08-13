ac84ce84cac028d6c40c5816b9ebe168ce2bf1b3
lauralall <lauralall2004@gmail.com>
2025-08-13 15:17:56 +0300

    justfilm header updated

--- ssg/source/home/script.js
+++ ssg/source/home/script.js
@@ -16,18 +16,38 @@ window.onscroll = function () {
 
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header');
-    let lastScrollTop = 0;
+    let lastScrollTop = 0
 
     window.addEventListener('scroll', () => {
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
       if (scrollTop > lastScrollTop && scrollTop > 100) {
-        header.classList.add('hide-on-scroll');
+        header.classList.add('hide-on-scroll')
       } else {
-        header.classList.remove('hide-on-scroll');
+        header.classList.remove('hide-on-scroll')
       }
 
       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-    });
-  });
+    })
 
+    function adjustMenu() {
+      const menu = document.querySelector('.org_menu')
+      if (!menu) return
+
+      menu.querySelectorAll('.org_item.small').forEach(item => item.style.display = '')
+
+      const smallItems = Array.from(menu.querySelectorAll('.org_item.small'))
+
+      for (let i = smallItems.length - 1; i >= 0; i--) {
+        if (menu.scrollWidth > menu.clientWidth) { 
+          smallItems[i].style.display = 'none'
+        } else {
+          break
+        }
+      }
+    }
+
+  adjustMenu()
+  window.addEventListener('load', adjustMenu)
+  window.addEventListener('resize', adjustMenu)
+})
